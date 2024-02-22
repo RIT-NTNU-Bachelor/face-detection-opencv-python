320b97c56c74e6ba55918878b53f365d50a5e86b
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-02-22 14:18:55 -0500

    refactor: moved code for better modularity + created function

---
+++ detect_face_from_image.py
@@ -0,0 +1,51 @@
+# OpenCV Library 
+import cv2
+
+# Uses OpenCV's built-in function for detecting a face.
+# image_path => path of the image.
+# Returns the image with green boxes around
+def detect_face_from_image(image_path="./images/4_faces.jpg"):
+    # Read the image
+    img = cv2.imread(image_path)
+
+    # Printing the image
+    print(f"Original Image dimension: ({img.shape})")
+
+
+    # Turing the image into a grayscale image
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Printing the gray scale image
+    print(f"Gray-Scale Image dimension: ({gray_image.shape})")
+
+    # Loading the classifier from a pretrained dataset
+    face_classifier = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+
+    # Performing the face detection
+    # - Using the method detectMultiScale() to detect faces
+    # - Using the grayscale image 
+    # - scaleFactor is for scaling down the input image
+    #   - Makes it easier to detect faces 
+    #   - Reducing size by 10%
+    # - minNeighbors is for setting the number of neighboring rectangles next to a object to make it valid
+    #   - Changing this number will reduce the amount of false positives
+    # - minSize is the minimum size of the object to be detected
+    face = face_classifier.detectMultiScale(
+        gray_image, scaleFactor=1.2, minNeighbors=10, minSize=(50,50)
+    )
+
+    # Drawing the squares from the given box
+    for (x, y, w, h) in face:
+        # Notify the user that a face has been detected
+        print("Processing face...")
+
+        # Using OpenCV function to create the rectangle at the given potion
+        color = (0, 255, 0) # Green
+        thickness = 6
+        cv2.rectangle(img, (x,y), (x+w,y+h), color, thickness) 
+
+
+    # Turning the color of the image back to normal and returning the image
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
--- main.py
+++ main.py
@@ -2,56 +2,33 @@
 import cv2
 import matplotlib.pyplot as plt
 
-# Path to the image with 
-image_path = "./images/4_faces.jpg"
+from detect_face_from_image import detect_face_from_image
 
-# Read the image
-img = cv2.imread(image_path)
+def run_program():
+    print("Options:\n")
+    print(" - [1] Detect face from a given image\n")
+    print(" - [2] Detect image from camera feed\n")
+    user_input = input("Please choose a option (1/2):")
 
-# Printing the image
-print(f"Original Image dimension: ({img.shape})")
-
-
-# Turing the image into a grayscale image
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# Printing the gray scale image
-print(f"Gray-Scale Image dimension: ({gray_image.shape})")
-
-# Loading the classifier from a pretrained dataset
-face_classifier = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-)
-
-# Performing the face detection
-# - Using the method detectMultiScale() to detect faces
-# - Using the grayscale image 
-# - scaleFactor is for scaling down the input image
-#   - Makes it easier to detect faces 
-#   - Reducing size by 10%
-# - minNeighbors is for setting the number of neighboring rectangles next to a object to make it valid
-#   - Changing this number will reduce the amount of false positives
-# - minSize is the minimum size of the object to be detected
-face = face_classifier.detectMultiScale(
-    gray_image, scaleFactor=1.2, minNeighbors=10, minSize=(50,50)
-)
-
-# Drawing the squares from the given box
-for (x, y, w, h) in face:
-    # Notify the user that a face has been detected
-    print("Processing face...")
-
-    # Using OpenCV function to create the rectangle at the given potion
-    color = (0, 255, 0) # Green
-    thickness = 6
-    cv2.rectangle(img, (x,y), (x+w,y+h), color, thickness) 
+    # Validate user input
+    if int(user_input) < 1 or int(user_input) > 2:
+        print("Incorrect usage of program.\n")
+        print(" - python3 main.py <OPTION> \n")
+        exit(1)
 
 
-# Turning the color of the image back to normal
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    if user_input.strip() == "1":
+        # Detect the face from the image
+        img = detect_face_from_image()
 
-# Displaying the image with pyplot 
-plt.figure()
-plt.imshow(img_rgb)
-plt.axis('off')
-plt.show()
+        # Displaying the image with pyplot 
+        plt.figure()
+        plt.imshow(img)
+        plt.axis('off')
+        plt.show()
+
+
+# Running the script
+if __name__ == "__main__":
+    run_program()
+
